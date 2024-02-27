65db00865fb93e235a9535dcd2bf90b04a0790b5
vicdsso <vicdsso@gmail.com>
2024-02-27 12:30:45 -0300

    aula2702

---
+++ basic/basictools/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import 'react' 
+
+//componentização semelhante a function 
+const NavBar =() =>{
+   return (
+    <>
+    <nav>
+        <ul>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#">Instituicional</a></li>
+            <li> <a href="#">Sobre nós</a></li>
+            <li> <a href="#">Notícias</a></li>
+        </ul>
+    </nav>
+    </>
+
+   ) 
+}
+
+
+export default NavBar
+
+
+
+
