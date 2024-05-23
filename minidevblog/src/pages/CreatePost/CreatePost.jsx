4aeee18bed8ca7c0be686fc81d832a8d8f468955
kamilyfatec <kamily.simeao@fatec.sp.gov.br>
2024-05-23 11:14:51 -0300

    Aula2105

---
+++ minidevblog/src/pages/CreatePost/CreatePost.jsx
@@ -0,0 +1,11 @@
+import styles from './CreatePost.module.css'
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+
+const CreatePost = () => {
+
+}
+
+export default CreatePost
---
+++ minidevblog/src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { userAuthentication } from '../../hooks/userAuthentication'
+
+const Register = () => {
+  
+  
+  //#endregion
+}
+
+export default Register
